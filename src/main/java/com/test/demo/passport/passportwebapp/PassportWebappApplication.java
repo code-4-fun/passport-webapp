21afea9b9dc6175589aeaeb43022a00c4f33ba43
Devendra Nalawade <devendra@Devendras-MacBook-Pro.local>
2017-07-17 01:45:13 +0900

    initial commit

---
+++ src/main/java/com/test/demo/passport/passportwebapp/PassportWebappApplication.java
@@ -0,0 +1,12 @@
+package com.test.demo.passport.passportwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassportWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassportWebappApplication.class, args);
+	}
+}
